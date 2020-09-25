4b599f0ddd78845db564dfd46ed581e06ab6e9a1
Benjamin O'Brien <bobrien9274@gmail.com>
2020-09-25 16:28:05 -0500

    Basic project checking

--- app/routes/uploading.py
+++ app/routes/uploading.py
@@ -1,6 +1,6 @@
 # Modules
 from app import app
-from flask import render_template, request
+from flask import render_template, request, abort
 
 # Routes
 @app.route("/upload", methods = ["GET"])
@@ -15,3 +15,16 @@ def upload():
 def cli_ping():
 
   return "200 OK", 200
+
+@app.route("/upload/cli/project", methods = ["POST"])
+def cli_getproject():
+
+  username = request.form.get("username")
+
+  project = request.form.get("name")
+
+  if not username or not project:
+
+    return abort(400)
+
+  return abort(403)  # temporary
